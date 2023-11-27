068db12740b0c40e27f6048dc49b5ec151091c92
Fernández Zavaleta <fz21i04001@usonsonate.edu.sv>
2023-11-26 23:16:55 -0300

    Correccion de vistas

--- Guia 1 2 y 3/VISTAS PROYECTO.sql
+++ Guia 1 2 y 3/VISTAS PROYECTO.sql
@@ -1,46 +1,62 @@
---Vista
--- Crear vista que une las tres tablas
-create view Departamento.VistaDireccion as
-select
-    d.Id_Departamento,
-    d.Departamento,
-    d.Pais,
-    m.Id_Municipio,
-    m.Municipio,
-    m.Id_Departamento as Municipio_Departamento,
-    di.Id_Distrito,
-    di.Distrito,
-    di.Id_Municipio as Distrito_Municipio
-from
-    Departamento.Departamentos d
-join
-    Departamento.Municipios m on d.Id_Departamento = m.Id_Departamento
-join
-    Departamento.Distritos di on m.Id_Municipio = di.Id_Municipio;
+CREATE VIEW DireccionCompleta AS
+SELECT
+	D.Id_Direccion,
+    D.Linea1,
+    D.Linea2,
+    D.CodigoPostal,
+    Dist.Distrito,
+    M.Municipio,
+    Dep.Departamento,
+    Dep.Pais
+FROM Departamento.Direcciones D
+JOIN Departamento.Distritos Dist ON D.Id_Distrito = Dist.Id_Distrito
+JOIN Departamento.Municipios M ON Dist.Id_Municipio = M.Id_Municipio
+JOIN Departamento.Departamentos Dep ON M.Id_Departamento = Dep.Id_Departamento;
+go
+select * from DireccionCompleta
 
+CREATE VIEW VistaEmpleadoDireccion AS
+SELECT
+    E.Id_Empleado,
+    E.DUI_Empleado,
+    E.ISSS_Empleado,
+    E.NombresEmpleado,
+    E.ApellidosEmpleado,
+    E.FechaNacEmpleado,
+    E.Telefono,
+    E.Correo,
+    E.Id_Cargo,
+    C.Cargo as CargoEmpleado,
+    E.Id_Direccion,
+    CONCAT(
+        DC.Linea1, ', ',
+        DC.Linea2, ', ',
+        DC.CodigoPostal, ', ',
+        DC.Distrito, ', ',
+        DC.Municipio, ', ',
+        DC.Departamento, ', ',
+        DC.Pais, ', ',
+        CAST(DC.CodigoPostal as char(5))
+    ) as Direccion
+FROM Persona.Empleados E
+JOIN Persona.Cargos C ON E.Id_Cargo = C.Id_Cargo
+JOIN DireccionCompleta DC ON E.Id_Direccion = DC.Id_Direccion;
+go
 
--- Crear la nueva vista
-create view Persona.VistaEmpleadosDireccion as
-select
-    e.DUI_Empleado,
-    e.ISSS_Empleado,
-    e.NombresEmpleado,
-    e.ApellidosEmpleado,
-    e.FechaNacEmpleado,
-    e.Telefono,
-    e.Correo,
-    c.Cargo as NombreCargo,
-    d.Linea1,
-    d.Linea2,
-    d.Id_Distrito,
-    d.CodigoPostal
-from
-    Persona.Empleados e
-join
-    Persona.Cargos c on e.Id_Cargo = c.Id_Cargo
-join
-    Departamento.Direcciones d on e.Id_Direccion = d.Id_Direccion;
+select * from  VistaEmpleadoDireccion
 
+CREATE VIEW VistaUsuarios AS
+SELECT
+    U.Id_Usuario,
+    U.Id_Empleado,
+    CONCAT(E.NombresEmpleado, ', ', E.ApellidosEmpleado) AS NombreCompleto,
+    E.Id_Cargo,
+    U.Id_Rol,
+    R.NombreRol,
+    U.Usuario,
+    U.Clave
+FROM Rol.Usuarios U
+JOIN Rol.Roles R ON U.Id_Rol = R.Id_Rol
+JOIN Persona.Empleados E ON U.Id_Empleado = E.Id_Empleado;
 
-	Select * from Persona.VistaEmpleadosDireccion
-
+select * from VistaUsuarios
